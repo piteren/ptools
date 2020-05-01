b346e55cac19e0ed3fb3b42ae28713c36a59492b
piteren <me@piotrniewinski.com>
2020-05-01 11:58:19 +0200

    args decorator update

--- lipytools/decorators.py
+++ lipytools/decorators.py
@@ -6,7 +6,7 @@
 
 """
 
-from inspect import getfullargspec, ismethod, isfunction
+from inspect import getfullargspec
 import time
 
 # decorator printing time report
@@ -18,85 +18,109 @@ def timing(f):
     new_f.__name__ = f'{f.__name__}:@timing'
     return new_f
 
-# prints debug info about parametersand given args/kwargs of function/method
-def args(f):
-    def new_f(*args, **kwargs):
-        ins = getfullargspec(f)
-        no_val = '--'
+# prints debug info about parameters and given args/kwargs of function/method, verb must be > 0 to print, if f got verb it overrides
+def args(verb):
+    def wrap(f):
+        def new_f(*args, **kwargs):
+            ins = getfullargspec(f)
+            no_val = '--'
 
-        arL = ins.args
-        if arL[0] == 'self': arL = arL[1:]              # simple, BUT not 100% accurate, ...but who names param 'self'?
+            arL = ins.args
+            if arL[0] == 'self': arL = arL[1:]              # simple, BUT not 100% accurate, ...but who names param 'self'?
 
-        defL = list(ins.defaults)                       # list of default values
-        defL = [no_val]*(len(arL)-len(defL)) + defL     # pad them with '--' from the beginning
-        arL = [list(e) for e in zip(arL,defL)]          # zip together
+            defL = list(ins.defaults)                       # list of default values
+            defL = [no_val]*(len(arL)-len(defL)) + defL     # pad them with no_val
+            arL = [list(e) for e in zip(arL,defL)]          # zip together
 
-        # append args from the beginning
-        if args:
-            arvL = args[1:]
-            for ix in range(len(arvL)):
-                v = arvL[ix]
-                arL[ix].append(v)
+            # append args from the beginning
+            if args:
+                arvL = args[1:]
+                for ix in range(len(arvL)):
+                    v = arvL[ix]
+                    arL[ix].append(v)
 
-        kwL = [[k,kwargs[k]] for k in kwargs]           # kwargs in list
+            kwL = [[k,kwargs[k]] for k in kwargs]           # kwargs in list
 
-        # get from kwargs params of f, append their values and remove from kwargs
-        kremIXL = []
-        for ix in range(len(kwL)):
+            # get from kwargs params of f, append their values and remove from kwargs
+            kremIXL = []
+            for ix in range(len(kwL)):
+                for e in arL:
+                    if kwL[ix][0]==e[0]:
+                        kremIXL.append(ix)
+                        e.append(kwL[ix][1])
+            for ix in reversed(kremIXL): kwL.pop(ix)
+
+            # add no value to not overridden params
             for e in arL:
-                if kwL[ix][0]==e[0]:
-                    kremIXL.append(ix)
-                    e.append(kwL[ix][1])
-        for ix in reversed(kremIXL): kwL.pop(ix)
+                if len(e)<3: e.append(no_val)
 
-        # add no value to not overridden params
-        for e in arL:
-            if len(e)<3: e.append(no_val)
+            my_verb = verb
+            for e in arL:
+                if e[0] == 'verb':
+                    v = e[2] if e[2] != no_val else e[1]
+                    my_verb = v
+                    break
 
-        # calc columns widths
-        kw = [10,10,10]
-        for e in arL:
-            for i in range(3):
-                lse = len(str(e[i]))
-                if lse>kw[i]: kw[i] = lse
-        for e in kwL:
-            for i in range(2):
-                lse = len(str(e[i]))
-                ki = i if not i else 2
-                if lse > kw[ki]: kw[ki] = lse
-        kw = [w+2 for w in kw]
+            if my_verb>0:
+                # calc columns widths, trim if
+                kw = [10,10,10]
+                for e in arL:
+                    for i in range(3):
+                        lse = len(str(e[i]))
+                        if lse>kw[i]: kw[i] = lse
+                for e in kwL:
+                    for i in range(2):
+                        lse = len(str(e[i]))
+                        ki = i if not i else 2
+                        if lse > kw[ki]: kw[ki] = lse
+                while sum(kw) > 120:
+                    mx = max(kw)
+                    for ix in range(3):
+                        if kw[ix] == mx: kw[ix] -= 1
+                mx = max(kw)
 
-        print(f'\n@args report of:  {f.__name__:40s} ********************************************')
-        print(f' > {"param":{str(kw[0])}s} {"default":{str(kw[1])}s} {"given":{str(kw[2])}s}')
-        for e in arL: print(f'   {str(e[0]):{str(kw[0])}s} {str(e[1]):{str(kw[1])}s} {str(e[2]):{str(kw[2])}s}')
-        print(f' > **kwargs (not used by {f.__name__}):')
-        for e in kwL: print(f'   {str(e[0]):{str(kw[0])}s} {"":{str(kw[1])}s} {str(e[1]):{str(kw[2])}s}')
-        print('@args report finished *********************************************************************************\n')
+                def s(p):
+                    r = str(p)
+                    if len(r) > mx: r = r[:mx-2] + '..'
+                    return r
 
-        f(*args, **kwargs)
+                print(f'\n@args report: *****************************************************************************************')
+                print(f' __name__: {f.__name__}')
+                print(f' > {"param":{str(kw[0])}s}  {"default":{str(kw[1])}s}  {"given":{str(kw[2])}s}')
+                for e in arL:
+                    print(f'   {s(e[0]):{str(kw[0])}s}  {s(e[1]):{str(kw[1])}s}  {s(e[2]):{str(kw[2])}s}')
+                print(f' > **kwargs (not used by {f.__name__}):')
+                for e in kwL:
+                    print(f'   {s(e[0]):{str(kw[0])}s}  {"":{str(kw[1])}s}  {s(e[1]):{str(kw[2])}s}')
+                print('@args report finished *********************************************************************************\n')
 
-    new_f.__name__ = f'{f.__name__}:@args'
-    return new_f
+            f(*args, **kwargs)
+
+        new_f.__name__ = f'{f.__name__}:@args'
+        return new_f
+    return wrap
 
 
 def example_args():
 
     class TestArgs:
 
-        @args
+        @args(1)
         def do_something(
                 self,
                 parameter_long_name_a,
                 pa_b,
                 pa_c,
-                def_a=  1,
+                def_a=  'sdgsegegassssssssssssssssssdfgasegsdffffsggggggggggggggggggggggggagsdgsdgsdfdddddddddddd',
                 def_b=  31.2434,
+                verb=   1,
                 **kwargs):
 
             print(parameter_long_name_a,pa_b,pa_c,def_a,def_b)
 
     exob = TestArgs()
-    exob.do_something(10, 11, pa_c=12, def_b=5, oth_a=6, first=0.000025)
+    d = {'agsgsdfgsdgsdgsdgsd':134,'sdghsdhsdhdghfghdfghdfghdfghdhg':12354,'asgagafsdgasdfasfasfasfasfasfasfdasdf':12345155}
+    exob.do_something(10, 11, pa_c=d, def_b=5, oth_a=6, first=0.000025)
 
 
 if __name__ == "__main__":
