e389859a1e65e65d2ac544ae25dd7354d2cec232
Piotr Niewinski <me@piotrniewinski.com>
2020-06-02 13:53:05 +0200

    print 3D for 1 dim

--- plots/three_dim.py
+++ plots/three_dim.py
@@ -48,21 +48,30 @@ def three_dim(
 if __name__ == '__main__':
 
     from ptools.neuralmess.layers import positional_encoding
+    width = 1#5
 
-    pe = positional_encoding(90,5,0.5,2,verb=1)
+    pe = positional_encoding(90, width, 0.9, 7, verb=1)
     pe = np.squeeze(pe)
 
     from ptools.plots.two_dim import two_dim
 
-    two_dim(pe[:, 0])
-    two_dim(pe[:, 1])
-    two_dim(pe[:,-2])
-    two_dim(pe[:,-1])
+    print(pe.shape)
+    if width == 1:
+        two_dim(pe)
+    else:
+        two_dim(pe[:, 0]) # first
+        #two_dim(pe[:, 1])
+        #two_dim(pe[:,-2])
+        two_dim(pe[:,-1]) # last
 
-    xyz = []
-    for rix in range(pe.shape[0]):
-        r = pe[rix]
-        for eix in range(pe.shape[1]):
-            xyz.append([rix,eix,pe[rix,eix]])
+    if width == 1:
+        xyz = []
+        for rix in range(pe.shape[0]):
+            xyz.append([rix, 0, pe[rix]])
+    else:
+        xyz = []
+        for rix in range(pe.shape[0]):
+            for eix in range(pe.shape[1]):
+                xyz.append([rix,eix,pe[rix,eix]])
 
     three_dim(xyz)
